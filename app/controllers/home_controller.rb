29a8970a469f24e309180ba69598a05d00a104fa
Zach Morek <zmorek@gmail.com>
2011-11-06 01:29:29 -0400

    omg wtf bbq eleven

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,20 @@
 class HomeController < ApplicationController
-  def index
-    url = URI.parse("http://www.sec.gov/Archives/edgar/data/1486800/000148680011000035/form10q.htm")
+  def get_html_content(requested_url)
+    url = URI.parse(requested_url)
     full_path = (url.query.blank?) ? url.path : "#{url.path}?#{url.query}"
     the_request = Net::HTTP::Get.new(full_path)
   
     the_response = Net::HTTP.start(url.host, url.port) { |http|
       http.request(the_request)
     }
-    
-    render :text => the_response.body, :layout => false 
+  
+    raise "Response was not 200, response was #{the_response.code}" if the_response.code != "200"
+    return the_response.body       
+  end  
+  def index
+    response1 = get_html_content("http://www.sec.gov/Archives/edgar/data/1486800/000148680011000035/form10q.htm")
+    response2 = get_html_content("http://www.sec.gov/Archives/edgar/data/1486800/000119312510255373/d10q.htm")
+    render :text => my_response, :layout => false 
     #respond_to do |format|
     #  format.html { render html: @the_response }
     #end
