b4030d16fd1a3fbaa2c46fc7d7b700ea03a51285
Kam Lasater <ckl@seekayel.com>
2011-11-06 01:48:43 -0400

    First step towards phase one structure

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,11 @@
+//# Place all the behaviors and hooks related to the matching controller here.
+//# All this logic will automatically be available in application.js.
+//# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$(document).ready(function() {
+	var d= new diff_match_patch;
+	var diffs=d.diff_main("txasdfasdt1","txt2");
+	d.diff_cleanupSemantic(diffs);
+	var out=d.diff_prettyHtml(diffs);
+	$('#content').html(out);
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,10 +10,17 @@ class HomeController < ApplicationController
   
     raise "Response was not 200, response was #{the_response.code}" if the_response.code != "200"
     return the_response.body       
-  end  
+  end
+  
   def index
-    response1 = get_html_content("http://www.sec.gov/Archives/edgar/data/1486800/000148680011000035/form10q.htm")
-    response2 = get_html_content("http://www.sec.gov/Archives/edgar/data/1486800/000119312510255373/d10q.htm")
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def results
+    @response1 = get_html_content("http://www.sec.gov/Archives/edgar/data/1486800/000148680011000035/form10q.htm")
+    @response2 = get_html_content("http://www.sec.gov/Archives/edgar/data/1486800/000119312510255373/d10q.htm")
     render :text => my_response, :layout => false 
     #respond_to do |format|
     #  format.html { render html: @the_response }
