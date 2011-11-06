86f0d947714275df1b5a3680fd7f72e0fd634ed0
Kam Lasater <ckl@seekayel.com>
2011-11-06 02:16:01 -0500

    Minimum Viable Product

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,11 +2,3 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-$(document).ready(function() {
-	var d= new diff_match_patch;
-	var diffs=d.diff_main($('#first').text(),$('#second').text());
-	d.diff_cleanupSemantic(diffs);
-	var out=d.diff_prettyHtml(diffs);
-	$('#content').html(out);
-});
- 
